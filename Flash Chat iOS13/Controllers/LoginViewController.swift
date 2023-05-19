e4f6068fab5870a46657d31446ec5382331aca93
Snooopyman <armando.snoop@gmail.com>
2023-05-19 19:06:13 +0200

    adding fuction for Login existing user in our Firebase DB

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-
-    @IBAction func loginPressed(_ sender: UIButton) {
-    }
     
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                
+                if let e = error {
+                    print(e)
+                } else {
+                    //Navigate to ChatVC
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+            
+        }
+        
+    }
 }
